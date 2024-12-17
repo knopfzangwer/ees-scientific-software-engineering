a5eb26a376a5847da1edc2e92f51042b7a43efe7
senzhan <senzhanopt@gmail.com>
2024-12-17 16:14:50 +0100

    lu init sen

---
+++ src/ees_scientific_software_engineering/lu_calc.py
@@ -0,0 +1,20 @@
+import numpy as np
+from scipy.linalg import lu_factor, lu_solve
+
+class LUSolver:
+    def __init__(self, input_matrix: np.ndarray):
+        """
+        Constructor of the class. It takes the input matrix and decompose it into LU factorization.
+        Store the factorization and permutation into class members.
+        """
+        lu, piv = lu_factor(input_matrix)
+        self.lu = lu
+        self.piv = piv
+
+
+    def solve(self, b: np.ndarray) -> np.ndarray:
+        """
+        Solve the linear equation with the input matrix and the given vector b.
+        """
+        x = lu_solve((self.lu, self.piv), b)
+        return x
